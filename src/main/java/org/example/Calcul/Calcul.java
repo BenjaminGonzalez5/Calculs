f8e7fe76abd5059ad650091b6a4e521caddd8b1b
BenjaminGonzalez5 <benjamin.gonzalez@epsi.fr>
2020-12-14 15:18:27 +0100

    On tente de réparer

---
+++ src/main/java/org/example/Calcul/Calcul.java
@@ -0,0 +1,32 @@
+package org.example.Calcul;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calcul 
+{
+	private int operande1 = 0;
+    private int operande2 = 0;
+    public Calcul(int operande1, int operande2)
+    {
+        this.operande1 = operande1;
+        this.operande2 = operande2;
+    }
+    public int multiplier()
+    {
+        return operande1 * operande2;
+    }
+    public int additionner()
+    {
+        return operande1 + operande2;
+    }
+    public int diviser()
+    {
+        return operande1 / operande2;
+    }
+    public int soustraire()
+    {
+        return operande1 - operande2;
+    }
+}
