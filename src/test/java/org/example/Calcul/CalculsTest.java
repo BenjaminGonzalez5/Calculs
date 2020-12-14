650612bab4d7ae43f3fdea65ebe61f3724b54eff
BenjaminGonzalez5 <benjamin.gonzalez@epsi.fr>
2020-12-14 16:41:17 +0100

    Peut-être ?

--- src/test/java/org/example/Calcul/CalculsTest.java
+++ src/test/java/org/example/Calcul/CalculsTest.java
@@ -1,8 +1,8 @@
 package org.example.Calcul;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class CalculsTest {
