ae16850d494646ba439d4ad87e7e143ba4d4542a
BenjaminGonzalez5 <43637769+BenjaminGonzalez5@users.noreply.github.com>
2020-12-14 14:58:11 +0100

    Update Calcul.java

--- src/main/java/org/example/Calcul/Calcul.java
+++ src/main/java/org/example/Calcul/Calcul.java
@@ -1,32 +1,27 @@
-package org.example.Calcul;
-
-/**
- * Hello world!
- *
- */
-public class Calcul 
+public class Calculs
 {
-	private int operande1 = 0;
-    private int operande2 = 0;
-    public void Calculs(int operande1, int operande2)
-    {
-        this.operande1 = operande1;
-        this.operande2 = operande2;
-    }
-    public int multiplier()
-    {
-        return operande1 * operande2;
-    }
-    public int additionner()
-    {
-        return operande1 + operande2;
-    }
-    public int diviser()
-    {
-        return operande1 / operande2;
-    }
-    public int soustraire()
-    {
-        return operande1 - operande2;
-    }
+	private final int operande1;
+	private final int operande2;
+
+	public Calculs(int operande1, int operande2)
+	{
+		this.operande1 = operande1;
+		this.operande2 = operande2;
+	}
+	public int multiplier()
+	{
+		return operande1 * operande2;
+	}
+	public int additionner()
+	{
+		return operande1 + operande2;
+	}
+	public int diviser()
+	{
+		return operande1 / operande2;
+	}
+	public int soustraire()
+	{
+		return operande1 - operande2;
+	}
 }
